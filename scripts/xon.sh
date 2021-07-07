05d94c9266ce1debb5cfa3148d406033894413e3
kuba <kubiernacki@gmail.com>
2021-07-07 11:58:39 +0200

    xdebug upgraded to 3.x

--- scripts/xon.sh
+++ scripts/xon.sh
@@ -7,7 +7,7 @@ HOST_PORT=$([ -n "$2" ] && echo $2 || echo 9000)
 
 mv /usr/local/etc/php/conf.d/xdebug.off /usr/local/etc/php/conf.d/xdebug.ini
 
-sed -i "s|xdebug.remote_host=.*|xdebug.remote_host=$HOST_IP|" $PHP_INI_DIR/conf.d/xdebug.ini
-sed -i "s|xdebug.remote_port=.*|xdebug.remote_port=$HOST_PORT|" $PHP_INI_DIR/conf.d/xdebug.ini
+sed -i "s|xdebug.client_host=.*|xdebug.client_host=$HOST_IP|" $PHP_INI_DIR/conf.d/xdebug.ini
+sed -i "s|xdebug.client_port=.*|xdebug.client_port=$HOST_PORT|" $PHP_INI_DIR/conf.d/xdebug.ini
 
 pkill -o -USR2 php-fpm
